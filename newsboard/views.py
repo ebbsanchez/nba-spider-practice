0f781019e2e5192f74d9897dfa81f65479cbebc5
Jack Sanchez <evo@gms.tku.edu.tw>
2020-03-02 10:32:42 +0800

    import restful api

--- newsboard/views.py
+++ newsboard/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Create your views here.
+from newsboard.models import NewsArticle
+from newsboard.serializers import NewsBoardSerializer
+
+from rest_framework import viewsets
+
+
+# Create your views here.
+class NewsBoardViewSet(viewsets.ModelViewSet):
+    queryset = NewsArticle.objects.all()
+    serializer_class = NewsBoardSerializer
